116ea22aa0766ff64636d4e579c1fcc2fc4598d8
Christian Hithe <chithe@uncc.edu>
2020-01-23 02:04:28 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,18 @@
+# Lab 1
+# Part I
+def sum arr
+  # YOUR CODE HERE
+  sum = 0 
+  arr.each{|a| sum+=a}
+  return sum
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
