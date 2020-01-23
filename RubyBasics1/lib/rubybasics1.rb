32af0e5f187836cbce4d923451eb1f75c3135019
Christian Hithe <chithe@uncc.edu>
2020-01-23 03:13:19 +0000

    RubyBasics1 - Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -10,10 +10,23 @@ end
 # Part II
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+  return arr[0]
+  end
   sum(arr.sort.last(2))
 end
 
 # Part III
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  for i in 0..arr.length-1
+for j in i+1..arr.length-1
+if (arr[i]+arr[j]==n)
+return true;
+end
+end
+  end
+return false;
 end
